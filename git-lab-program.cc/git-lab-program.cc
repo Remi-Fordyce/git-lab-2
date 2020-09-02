cc7dd5a517b46968575e0b4a62255b04171c9936
Remi Fordyce <rf510517@ohio.edu>
2020-09-02 18:07:05 -0400

    Hello Git

---
+++ git-lab-program.cc/git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Remi Fordyce
+ *        Date: 9/2/20
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+
+    /*add code*/
+    return 0;
+}// main
